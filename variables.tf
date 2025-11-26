b02e38ac40337fb44581e396d39aed0e692cb7b9
Mayank-kaneriya <mayankkaneriya7@gmail.com>
2025-11-26 21:44:33 +0530

    Add Terraform variables for region, CIDR, AMI, and instance type

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "ami" {
+  default = "ami-0261755bbcb8c4a84"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
